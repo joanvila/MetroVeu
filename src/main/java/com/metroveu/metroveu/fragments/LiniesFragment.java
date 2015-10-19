c22f15bff067ccc47587c47e940b6e03cac2154c
Esteve Torrents Lapuerta <esteve.torrents@est.fib.upc.edu>
2015-10-19 12:48:37 +0200

    llista parades

--- src/main/java/com/metroveu/metroveu/fragments/LiniesFragment.java
+++ src/main/java/com/metroveu/metroveu/fragments/LiniesFragment.java
@@ -5,8 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.metroveu.metroveu.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by joanvila on 14/10/15.
@@ -15,6 +19,21 @@ public class LiniesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.linies_fragment, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation", "Simple List View In Android",
+                "Create List View Android", "Android Example",
+                "List View Source Code", "List View Array Adapter",
+                "Android Example List View" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity()
+                .getApplicationContext(), android.R.layout.simple_list_item_1,
+                android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
 
         return rootView;
     }
