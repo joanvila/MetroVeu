d7d765f87064901405dcb96b368dfd91f5936246
Laura Carrio <laura.carrio@est.fib.upc.edu>
2015-11-05 13:50:09 +0100

    Acabada primera part fragments tarifes

--- src/main/java/com/metroveu/metroveu/fragments/RateFragment.java
+++ src/main/java/com/metroveu/metroveu/fragments/RateFragment.java
@@ -2,13 +2,16 @@ package com.metroveu.metroveu.fragments;
 
 import android.app.Fragment;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.metroveu.metroveu.R;
 import com.metroveu.metroveu.data.MetroDbHelper;
@@ -19,23 +22,24 @@ import java.util.ArrayList;
  * Created by Carla on 05/11/2015.
  */
 public class RateFragment extends Fragment{
-    private FragmentTransaction ft;
+    private android.app.FragmentTransaction ft;
     ArrayList<String> tarifaData;
 
-    //falta moificar el onCreateView
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.rates_fragment, container, false);
 
-        Bundle paradesBundle = getArguments();
-        final String tipusTarifa = paradesBundle.getString("tarifa_tipus");
+        Bundle tarifaBundle = getArguments();
+        final String tipusTarifa = tarifaBundle.getString("tarifa_tipus");
 
-        /*Cursor tarifes = new MetroDbHelper(getActivity().getApplicationContext()).getReadableDatabase().
-                rawQuery("select * from tarifa whre tarifa_tipus=?", null);
+        Cursor tarifes = new MetroDbHelper(getActivity().getApplicationContext()).getReadableDatabase().
+                rawQuery("select * from tarifa whre tarifa_tipus=?", new String[]{tipusTarifa});
+
         tarifaData = new ArrayList<>();
+
         if(tarifes != null && tarifes.moveToFirst()) {
             do {
-                tarifaData.add(tarifes.getString(tarifes.getColumnIndex("tarifa_tipus")));
+                tarifaData.add(tarifes.getString(tarifes.getColumnIndex("tarifa_nom")));
             } while (tarifes.moveToNext());
             tarifes.close();
         }
@@ -46,17 +50,17 @@ public class RateFragment extends Fragment{
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
-                RatesFragment ratesFragment = new RatesFragment();
+                RateFragment rateFragment = new RateFragment();
                 Bundle tarifesBundle = new Bundle();
-                tarifesBundle.putString("tarifa_tipus", tarifesData.get(position));
-                ratesFragment.setArguments(tarifesBundle);
+                tarifesBundle.putString("tarifa_nom", tarifaData.get(position));
+                rateFragment.setArguments(tarifesBundle);
                 ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.content_frame, ratesFragment);
+                ft.replace(R.id.content_frame, rateFragment);
                 ft.addToBackStack(null);
                 ft.commit();
             }
-        });*/
-
+        });
         return rootView;
     }
+
 }
