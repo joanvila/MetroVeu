2cf39681ac2ce5ff2a6c47083a85a07190272596
carladivicuesta <carla.divi@est.fib.upc.edu>
2015-11-12 19:29:27 +0100

    Tarifes fetes

--- src/main/java/com/metroveu/metroveu/fragments/RateInfoFragment.java
+++ src/main/java/com/metroveu/metroveu/fragments/RateInfoFragment.java
@@ -7,37 +7,54 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.TextView;
 
 import com.metroveu.metroveu.R;
 import com.metroveu.metroveu.data.MetroDbHelper;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Created by Carla on 12/11/2015.
  */
 public class RateInfoFragment extends Fragment {
     private android.app.FragmentTransaction ft;
-    ArrayList<String> tarifaData;
+    private TextView nomTarifaView = null;
+    private TextView descripcioTarifaView = null;
+    private TextView preuTarifaView = null;
+    private String descrT;
+    private String preuT;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.rate_fragment, container, false);
 
+
         Bundle tarifaBundle = getArguments();
-        final String tipusTarifa = tarifaBundle.getString("tarifa_tipus");
+        final String nomT= tarifaBundle.getString("tarifa_nom");
+        //final String descrT = tarifaBundle.getString("tarifa_descripcio");
+        //final String preuT = tarifaBundle.getString("tarifa_preu");
 
-        Cursor tarifes = new MetroDbHelper(getActivity().getApplicationContext()).getReadableDatabase().
-                rawQuery("select * from tarifa where tarifa_tipus=?", new String[]{tipusTarifa});
+        Cursor tarifa = new MetroDbHelper(getActivity().getApplicationContext()).getReadableDatabase().
+                rawQuery("select * from tarifa where tarifa_nom =?", new String[]{nomT});
 
-        tarifaData = new ArrayList<>();
-
-        if (tarifes != null && tarifes.moveToFirst()) {
-            do {
-                tarifaData.add(tarifes.getString(tarifes.getColumnIndex("tarifa_nom")));
-            } while (tarifes.moveToNext());
-            tarifes.close();
+        if (tarifa != null && tarifa.moveToFirst()){
+            descrT = tarifa.getString(tarifa.getColumnIndex("tarifa_descripcio"));
+            preuT = tarifa.getString(tarifa.getColumnIndex("tarifa_preu"));
+            tarifa.close();
         }
+
+
+        nomTarifaView = (TextView) rootView.findViewById(R.id.nomTarifa);
+        nomTarifaView.setText(nomT);
+        descripcioTarifaView = (TextView) rootView.findViewById(R.id.descripcioTarifa);
+        descripcioTarifaView.setText(descrT);
+        preuTarifaView = (TextView) rootView.findViewById(R.id.preuTarifa);
+        preuTarifaView.setText(preuT);
+
         return rootView;
 
     }
