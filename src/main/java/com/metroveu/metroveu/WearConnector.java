8bb9df6d98888dc39589bcc118a1ed5e2ff04ae5
joanvila <vila.joan94@gmail.com>
2015-12-03 19:48:59 +0100

    Service to recieve wear messages

---
+++ src/main/java/com/metroveu/metroveu/WearConnector.java
@@ -0,0 +1,29 @@
+package com.metroveu.metroveu;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class WearConnector extends WearableListenerService {
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent dataEvent : dataEvents) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if (path.equals("/lines")) {
+                    int number = dataMap.getInt("number");
+                    Log.v("wear", Integer.toString(number));
+                }
+            }
+        }
+    }
+}
